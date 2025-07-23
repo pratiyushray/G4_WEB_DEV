94683a557417d80bf488c0b35118359a286ed674
Pratiyush Ray <pkray0021@gmail.com>
2025-07-23 15:49:11 +0530

    NPM PACKAGE: CHANGESET

--- L8_MODULES/index.js
+++ L8_MODULES/index.js
@@ -1,18 +1,25 @@
-console.log("SCRIPT JS");
+// console.log("SCRIPT JS");
 
-const {sumOfTwo,xy} = require("./calculateTwo/sumOfTwo");
-const mulOfTwo = require("./calculateTwo/mulOfTwo");
-const divOfTwo = require("./calculateTwo/divOfTwo")
+// const {sumOfTwo,xy} = require("./calculateTwo/sumOfTwo");
+// const mulOfTwo = require("./calculateTwo/mulOfTwo");
+// const divOfTwo = require("./calculateTwo/divOfTwo")
 
-// console.log(xy)
+// // console.log(xy)
 
-// var xy =30000;
+// // var xy =30000;
 
-// CALCULATOR
+// // CALCULATOR
 
 
 
-console.log("XY",xy)
-sumOfTwo(10,20);
-mulOfTwo(10,20);
-divOfTwo(40,10);
+// console.log("XY",xy)
+// sumOfTwo(10,20);
+// mulOfTwo(10,20);
+// divOfTwo(40,10);
+
+
+// const { addTwoNumber, subTwoNumber, divTwoNumber} = require("g4-npm-package");
+
+// console.log(addTwoNumber());
+// console.log(subTwoNumber());
+// console.log(divTwoNumber());
